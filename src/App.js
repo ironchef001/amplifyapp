4c548efc2bffd3c6b8466a08fd6cf56132c2c1c3
ironchef001 <ironchef001@yahoo.com>
2021-08-13 22:15:46 -0500

    auth off

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
+        <h1>No Auth!</h1>
       </header>
-      <AmplifySignOut />
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
+// export default withAuthenticator(App);
